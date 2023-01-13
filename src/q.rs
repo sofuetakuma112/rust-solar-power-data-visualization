ffafe7a1e503d2cf659df9ede038d481a7c02261
sofuetakuma <kawahagi0620@gmail.com>
2023-01-13 16:19:30 +0900

    不要なコメント削除

--- src/q.rs
+++ src/q.rs
@@ -7,28 +7,15 @@ pub fn calc_q(dt: &DateTime<Local>, lat_deg: f64, lng_deg: f64) -> f64 {
     let dn = (dt_delta.num_days() + 1) as f64;
     let theta = 2.0 * PI * (dn - 1.0) / 365.0;
 
-    // println!("dn: {}", dn);
-    // println!("theta: {}", theta);
-
     // 太陽赤緯(単位はラジアン)
     let theta_2x = 2.0 * theta;
     let theta_3x = 3.0 * theta;
-
-    // println!("theta.sin(): {}", theta.sin());
-    // println!("theta_2x.sin(): {}", theta_2x.sin());
-    // println!("theta_3x.sin(): {}", theta_3x.sin());
-
-    // println!("theta.cos(): {}", theta.cos());
-    // println!("theta_2x.cos(): {}", theta_2x.cos());
-    // println!("theta_3x.cos(): {}", theta_3x.cos());
 
     let delta = 0.006918 - (0.399912 * theta.cos()) + (0.070257 * theta.sin())
         - (0.006758 * theta_2x.cos())
         + (0.000907 * theta_2x.sin())
         - (0.002697 * theta_3x.cos())
         + (0.001480 * theta_3x.sin());
-
-    // println!("delta: {}", delta);
 
     // 地心太陽距離のルートの中身
     let geocentri_distance_like = 1.000110
@@ -37,14 +24,11 @@ pub fn calc_q(dt: &DateTime<Local>, lat_deg: f64, lng_deg: f64) -> f64 {
         + 0.000719 * theta_2x.cos()
         + 0.000077 * theta_2x.sin();
 
-    // println!("geocentri_distance_like: {}", geocentri_distance_like);
-
     // 均時差
     let eq = 0.000075 + 0.001868 * theta.cos()
         - 0.032077 * theta.sin()
         - 0.014615 * theta_2x.cos()
         - 0.040849 * theta_2x.sin();
-    // println!("eq: {}", eq);
 
     let phi = lat_deg * PI / 180.0;
 
